877ac4f8380f878d960c13c0d203e9feb00a9193
Stefan Zott <stefan@familiezott.de>
2025-04-25 16:28:14 +0200

    localStorage wird als Datenbank verwendet. Alle Kleiderspenden werden in der Datenbank abgespeichert

--- src/pages/RegistrationScreen.tsx
+++ src/pages/RegistrationScreen.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useRef } from "react";
 import { useHandover } from "../context/FormContext";
 
 function RegistrationScreen() {
+    const hasRun = useRef(false);
     const { data } = useHandover();
+
+    useEffect(() => {
+        if (hasRun.current) return;
+        hasRun.current = true;
+        
+        let id = localStorage.length + 1
+
+        const jsonData = JSON.stringify(data);
+        localStorage.setItem(id.toString(), jsonData);
+    })
 
     return (
         <div className="container mt-4">
